af353f3598b3678cee4913b9e4e4fb990c0c23c9
SchiavoniDaniele <167100380+SchiavoniDaniele@users.noreply.github.com>
2024-06-14 15:54:58 +0200

    Update number_script.js

--- src/number_script.js
+++ src/number_script.js
@@ -5,11 +5,10 @@ start_modal.show();
 function showGame() {
     document.getElementById("game_screen").removeAttribute("hidden");
 }
-//Variabili globali per il numero vincente, i tentativi massimi e il settaggio dei record
+//Variabili globali per il numero vincente e i tentativi massimi
 var lucky_number = 0;
 var max_tries = 0;
-
-
+var record = 0;
 //Questo blocco di codice mi serve a bloccare la funzione di base del tasto enter, consentendo invece di inviare il dato al gioco
 document.getElementById("number").addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
@@ -23,16 +22,16 @@ function newGame() {
     
     lucky_number = Math.floor(Math.random() * 101); //Per ottenere un numero intero casuale tra 0 e 100
     max_tries = 10; //reset tentativi e indizi
-    
+    document.getElementById("hint").innerHTML = "";
     document.getElementById("game_visibility").removeAttribute("hidden");
     document.getElementById("game_screen").removeAttribute("hidden");
-    document.getElementById("tries_left").innerHTML = max_tries;
-    document.getElementById("hint").innerHTML = "";
-    document.getElementById("record_name").value=""; 
-    //document.getElementById("hint").innerHTML=lucky_number; //Solo per i test, poi va tolta
+    document.getElementById("lose_screen").setAttribute("hidden", "");
+    document.getElementById("tries_left").textContent = max_tries;
+    document.getElementById("hint").textContent = lucky_number; //Solo per i test, poi va tolto
 }
 
 function guessNumber() {
+    
     var number = document.getElementById("number").value;
     document.getElementById("tries_left").innerHTML = max_tries;
     document.getElementById("number").value = ""; //Per maggior fluidità, a ogni tentativo il numero scritto viene cancellato
@@ -42,15 +41,13 @@ function guessNumber() {
             
             switch (true) {
                 case number == lucky_number:
-                    
                     // Mostra il modale di vittoria
-                    var win_modal = bootstrap.Modal.getOrCreateInstance('#win_modal');
+                    var win_modal = new bootstrap.Modal(document.getElementById("win_modal"));
                     win_modal.show();
-                    if(parseInt(document.getElementById("record3").innerText)>=parseInt(max_tries))
-                    {
-                                                    document.getElementById("record_screen").setAttribute("hidden","");                                 
-                    }
                     document.getElementById("game_visibility").setAttribute("hidden", "");
+                    if (max_tries > parseInt(document.getElementById("record1").innerText)) {
+                        getRecord()
+                    }
                     break;
                 case number > lucky_number:
                     document.getElementById("hint").innerHTML = "Il numero che cerchi è più piccolo!";
@@ -80,60 +77,61 @@ function guessNumber() {
 }
 
 
-function getRecord(){
-    let record_name=document.getElementById("record_name").innerText;
-    
-    switch(true){
-    //Caso 1: il giocatore batte tutti i record
-        case max_tries>parseInt(document.getElementById("record1").innerText):
-       //aggiorna i punteggi 
-       document.getElementById("record3").innerText=document.getElementById("record2").innerHTML;
-        document.getElementById("record2").innerText=document.getElementById("record1").innerHTML;
-        document.getElementById("record1").innerText=max_tries;
-        
-        //aggiorna i nomi
-        document.getElementById("record_name3").innerText=document.getElementById("record_name2").innerHTML;
-        document.getElementById("record_name2").innerText=document.getElementById("record_name1").innerHTML;
-        document.getElementById("record_name1").innerText=document.getElementById("record_name").value;
-        
-        //aggiorna il numero vincente
-        document.getElementById("lucky_number_record3").innerText=document.getElementById("lucky_number_record2").innerHTML;
-        document.getElementById("lucky_number_record2").innerText=document.getElementById("lucky_number_record1").innerHTML;
-        document.getElementById("lucky_number_record1").innerText=lucky_number;
-        break;
-        
+function getRecord() {
+    let record_name = document.getElementById("record_name").innerText;
+    document.getElementById("record_screen").removeAttribute("hidden");
+
+    switch (true) {
+        //Caso 1: il giocatore batte tutti i record
+        case max_tries > parseInt(document.getElementById("record1").innerText):
+            //aggiorna i punteggi 
+            document.getElementById("record3").innerText = document.getElementById("record2").innerHTML;
+            document.getElementById("record2").innerText = document.getElementById("record1").innerHTML;
+            document.getElementById("record1").innerText = max_tries;
+
+            //aggiorna i nomi
+            document.getElementById("record_name3").innerText = document.getElementById("record_name2").innerHTML;
+            document.getElementById("record_name2").innerText = document.getElementById("record_name1").innerHTML;
+            document.getElementById("record_name1").innerText = document.getElementById("record_name").value;
+
+            //aggiorna il numero vincente
+            document.getElementById("lucky_number_record3").innerText = document.getElementById("lucky_number_record2").innerHTML;
+            document.getElementById("lucky_number_record2").innerText = document.getElementById("lucky_number_record1").innerHTML;
+            document.getElementById("lucky_number_record1").innerText = lucky_number;
+            break;
+
         //Caso 2: il giocatore batte gli ultimi due record
-        case  max_tries>parseInt(document.getElementById("record2").innerText):
-       //aggiorna i punteggi 
-       document.getElementById("record3").innerText=document.getElementById("record2").innerHTML;
-        document.getElementById("record2").innerText=max_tries;
-        
-        //aggiorna i nomi
-        document.getElementById("record_name3").innerText=document.getElementById("record_name2").innerHTML;
-        document.getElementById("record_name2").innerText=document.getElementById("record_name").value;
-        
-        
-        //aggiorna il numero vincente
-        document.getElementById("lucky_number_record3").innerText=document.getElementById("lucky_number_record2").innerHTML;
-        document.getElementById("lucky_number_record2").innerText=lucky_number;
-        break;
-        
-        
+        case max_tries > parseInt(document.getElementById("record2").innerText):
+            //aggiorna i punteggi 
+            document.getElementById("record3").innerText = document.getElementById("record2").innerHTML;
+            document.getElementById("record2").innerText = max_tries;
+
+            //aggiorna i nomi
+            document.getElementById("record_name3").innerText = document.getElementById("record_name2").innerHTML;
+            document.getElementById("record_name2").innerText = document.getElementById("record_name").value;
+
+
+            //aggiorna il numero vincente
+            document.getElementById("lucky_number_record3").innerText = document.getElementById("lucky_number_record2").innerHTML;
+            document.getElementById("lucky_number_record2").innerText = lucky_number;
+            break;
+
+
         //Caso 3: il giocatore batte l'ultimo record
         default:
-       //aggiorna i punteggi 
-       document.getElementById("record3").innerText=max_tries;
-        
-        //aggiorna i nomi
-        document.getElementById("record_name3").innerText=document.getElementById("record_name").value;
-        
-        
-        
-        //aggiorna il numero vincente
-        document.getElementById("lucky_number_record3").innerText=lucky_number;
-        
-    }
-    
-   
-}
+            //aggiorna i punteggi 
+            document.getElementById("record3").innerText = max_tries;
 
+            //aggiorna i nomi
+            document.getElementById("record_name3").innerText = document.getElementById("record_name").value;
+
+
+
+            //aggiorna il numero vincente
+            document.getElementById("lucky_number_record3").innerText = lucky_number;
+           
+
+    }
+
+
+}
